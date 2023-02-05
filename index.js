39e345f2fcf4c380a80f90cfb8312d989e592406
Laurence Fairbanks <laurencefairbanks@yahoo.com>
2023-02-04 22:31:20 -0600

    commit tree be37a3bd45776efbf4778ce20d9d109cfe5e96df

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+    let newArray = []
+  for (let i = 0; i < names.length; i++) {
+   newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return newArray
+  }
+
+ function countDown(x) {
+    let ticker = x;
+    while (ticker >= 0) {
+        console.log(ticker--)
+    }
+    return ticker
+}
+
